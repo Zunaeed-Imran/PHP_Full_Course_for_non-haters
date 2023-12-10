20b68b7d0af2e4a74911b7d4761393c3d4dce16f
imran <zunaeede@gmail.com>
2023-12-10 16:11:03 +0600

    main - add a new example

---
+++ 2_food.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="" method="post">
+    <h2>Order Only Pizza</h2><br>
+    <label>Enter Quentiy</label><br>
+    <input type="text" name="quentity" placeholder="Pizza Quentity"><br>
+    <input type="submit" name="submit">
+  </form>
+</body>
+
+</html>
+
+<?php
+// Simple food quentity example not using localhost database
+
+$foob = "Pizza";
+$quentity = $_POST['quentity'];
+$total = $quentity * 5.33;
+
+echo "You have Order {$quentity} x Pizza <br>";
+echo "And the total Price is {$total} $";
+
+?>
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# PHP_Full_Course_for_non-haters
-### PHP Full Course for non-haters From Bro code you tube channe;
-- [Video Tutorial of this reppo](https://youtu.be/zZ6vybT1HQs?si=Tduc5vDxj-vLaZMG)
-1. Variable (13:58 in video)
-- practice variable
+# PHP_Full_Course_for_non-haters.
+### PHP Full Course for non-haters From Bro code you tube channe.
+- [Video Tutorial of this reppo](https://youtu.be/zZ6vybT1HQs?si=Tduc5vDxj-vLaZMG).
+1. Variable (13:58 in video).
+- practice variable.
+2. $_GET or $_POST Method (45:13 in video).
+- example of a food price calculate.
+- not using localhost or any database only out_put tha input value with calculted.
