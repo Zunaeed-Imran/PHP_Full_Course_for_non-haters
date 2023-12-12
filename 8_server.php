b6742af3056e4dee0f12491c5317fc78519770cf
imran <zunaeede@gmail.com>
2023-12-12 17:17:49 +0600

    main - server request method

--- 8_server.php
+++ 8_server.php
@@ -1,8 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+    <input type="text" name="name"><br>
+    <input type="submit" name="login"><br>
+  </form>
+</body>
+</html>
+
 <?php
 
-foreach($_SERVER as $key => $value){
-  echo "{$key} = {$value} <br>";
+// foreach($_SERVER as $key => $value){
+//   echo "{$key} = {$value} <br>";
+// }
+
+if($_SERVER['REQUEST_METHOD' == 'POST']){
+  echo"Hi";
 }
+
+
+
+
 
 // with server super global we can see everything 
 // for now see the `request mathod` and `PHP_SELF`
