614fc9eef3c3e0701c847682e84ed45803d4aa76
imran <zunaeede@gmail.com>
2023-12-12 11:50:38 +0600

    main - session input value try

---
+++ 7.3_session.php
@@ -0,0 +1,17 @@
+<?php
+// session input value login page
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Session input value login page</title>
+</head>
+<body>
+  
+</body>
+</html>
